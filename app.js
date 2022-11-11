3fe084b4348c498b6f4f5e506b9f9f9899c5cf17
Kevin Garces Høyberg <68248803+kevinghberg@users.noreply.github.com>
2022-11-11 11:29:56 -0300

    change port to be dynamically assigned by heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = require('./router')
 require("dotenv").config()
 const app = express()
-const port = 3000
+const port = process.env.PORT
 app.use('/', router);
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port} 🚀🚀🚀🚀`)
